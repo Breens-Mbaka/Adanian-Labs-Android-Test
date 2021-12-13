3169ab5e16693bd5452758bef9dcf8c8490c8155
user <user@users-MacBook-Air.local>
2021-12-13 15:56:05 +0300

    Added navigation graph

---
+++ app/src/main/java/com/breens/adaninalabsandroidtets/ui/fragments/DetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.breens.adaninalabsandroidtets.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.breens.adaninalabsandroidtets.R
+
+class DetailsFragment:Fragment(R.layout.fragment_details) {
+}
---
+++ app/src/main/java/com/breens/adaninalabsandroidtets/ui/fragments/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.breens.adaninalabsandroidtets.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.breens.adaninalabsandroidtets.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
